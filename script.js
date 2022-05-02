e3003ea4af560f5a2e8fbc58aa9d29abc543ab36
Rozles <rozlesterle@gmail.com>
2022-05-02 22:54:11 +0200

    nc ne dela

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-var Nx = 301;
-var Nt = 100000;
+var Nx = 100;
+var Nt = 1000;
 var x = makeArr(0, 1, Nx);
 var dx = 1/(Nx-1);
-var dt=1e-7;
+var dt=1 / Nt;
 var sinusni = false;
+var psi0 = [];
+var stevec = 0;
 
-
+const psi = new Array(Nt);
+for (var r = 0; r < Nt; r++) {
+    psi[r] = new Array(Nx);
+    psi[r][0] = {re: 0, im: 0};
+}
 
 function nalozi() {
     document.getElementById("Nx").value = Nx;
     document.getElementById("Nt").value = Nt;
-
-    racunaj();
-   
+    psi0 = sestaviPsi();
 }
 
 function nakljucniPsi(){
     sinusni = false;
-    racunaj()
+    psi0 = sestaviPsi();
 }
 
 function sinusniPsi(){
     sinusni = true;
-    racunaj()
+    psi0 = sestaviPsi();
 }
 
-function narisiGrafPsi(psi) {
+function narisiGrafPsi(func) {
     var grafPsi = [];
-    for (var i = 0; i < psi.length; i++) {
-        var tocka = {x: x[i], y: psi[i], label: x[i]}
+    for (var i = 0; i < func.length; i++) {
+        var tocka = {x: x[i], y: func[i], label: x[i]}
         grafPsi[i] = tocka;
     }
 
@@ -52,16 +56,15 @@ function narisiGrafPsi(psi) {
     chart.render();
 }
 
-function integral(psi) {
+function integral(func) {
     var vsota = 0
     for (var i = 0; i < Nx; i++) {
-        vsota += psi[i] * psi[i] * dx;
+        vsota += func[i] * func[i] * dx;
     }
     return vsota;
 }
 
 function sestaviPsi(){
-    var psi0 = [];
     if(sinusni) {
         for(var i = 0; i < Nx; i++) {
             psi0[i] = Math.sqrt(2)*Math.sin(Math.PI*x[i]);
@@ -90,30 +93,60 @@ function posodobiParametre() {
 
 function racunaj() {
     posodobiParametre()
-    var psi0 = sestaviPsi();
-    const psi = new Array(Nt);
-    for (var i = 0; i < Nt; i++) {
-        psi[i] = new Array(Nx);
-    }
-
+    
     var psiComplex = [];
 
-    for (var j = 0; j < Nx; j++) {
-        psiComplex[j] = math.complex(psi0[j], 0);
+    for (var k = 0; k < Nx; k++) {
+        psiComplex[k] = {re: psi0[k], im: 0};
     }
     psi[0] = psiComplex;
 
-    console.log(psi[0][1]);
-
     for(var t = 0; t < Nt - 1; t++) {
-        for(var i = 1; i < Nx - 1; i++) {
-            psi[t + 1][i] = math.add(psi[t][i], math.multiply(math.multiply(math.complex(0, 1/2), dt/(dx * dx)), math.add(math.add(psi[t][i - 1], math.multiply(-1, math.multiply(2, psi[t][i]))), psi[t][i-1])));
-            
+        psi[t][Nx] = {re: 0, im: 0};
+        for(var i = 1; i < Nx; i++) {
+            psi[t + 1][i] = {re: psi[t][i].re + (-1 * (dt / 2 * dx * dx) * (psi[t][i + 1].im - 2 * psi[t][i].im + psi[t][i - 1].im)), im: psi[t][i].im + (psi[t][i + 1].re - 2 * psi[t][i].re + psi[t][i - 1].re) * (dt / 2 * dx * dx)};  
+        }
+        var normalizacija = integral(psi[t + 1]);
+        for (var j = 1; j < Nx; j++) {
+            psi[t + 1][j].re = psi[t + 1][j].re / normalizacija;
+            psi[t + 1][j].im = psi[t + 1][j].im / normalizacija;
+            psi[t + 1][i].y = psi[t + 1][i].re;
+            psi[t + 1][i].x = x[i];
         }
     }
 
-    console.log(psi);
+    console.log(psi)
+
+    animacijaGraf();
+    document.getElementById("next").hidden = false;
 }
+
+function animacijaGraf() {
+    var chart = new CanvasJS.Chart("chartContainer2", {
+        animationEnabled: false,  
+        title:{
+            text: "Zacetna valovna funkcija: psi0"
+        },
+        axisX: {
+            minimum: 0,
+            maximum: 1,
+        },
+        data: [{
+            yValueFormatString: "#,###",
+            xValueFormatString: "#,###",
+            type: "spline",
+            dataPoints: psi[stevec]
+        }]
+    });
+    chart.render();
+}
+
+function next() {
+    stevec++;
+    stevec = stevec % Nt;
+    animacijaGraf();
+}
+
 
 function makeArr(startValue, stopValue, cardinality) {
     var arr = [];
